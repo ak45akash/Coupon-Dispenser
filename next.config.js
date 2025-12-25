9d4677df03861c6ebceef6a9efbd518637445416
Akashdeep Kancha <ak45.akashdeep@gmail.com>
2025-12-25 18:09:41 +0530

    Add CORS headers for widget-embed.js to fix cross-origin access

--- next.config.js
+++ next.config.js
@@ -17,9 +17,17 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
         ],
       },
+      {
+        source: '/widget-embed.js',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
+          { key: 'Content-Type', value: 'application/javascript' },
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+        ],
+      },
     ]
   },
 }
 
 module.exports = nextConfig
-
